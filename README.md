b6d52314571a094b5aac4cc47852949a927c0b2e
Kris <kris.bebbington@inquisitive.com>
2024-03-11 22:14:11 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Outcomes frontend with material design
+
+A basic frontend for the outcomes project using material design.
+
+## Installation
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+npm start
+```
+
+## Build
+
+```bash
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// index.js
+import "@material/web/button/filled-button.js";
+import "@material/web/button/outlined-button.js";
+import "@material/web/chips/chip-set.js";
+import "@material/web/chips/input-chip.js";
+import "@material/web/chips/suggestion-chip.js";
+import "@material/web/chips/filter-chip.js";
+import "@material/web/textfield/outlined-text-field.js";
+import "@material/web/textfield/filled-text-field.js";
